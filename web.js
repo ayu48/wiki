508f02700eaf1b11f4c9d4f47f5187f9925c6311
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-04-22 00:35:58 +0900

    added index

--- web.js
+++ web.js
@@ -1,14 +1,20 @@
+// Module dependencies
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var site = require('./site');
 
+// Config
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'hbs');
 app.use(logfmt.requestLogger());
 
+// General
 app.get('/', function(req, res) {
-        res.send('Hello World!');
-        });
+    res.render('index', {title: 'Ayu\'s Blog'});
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
         console.log("Listening on " + port);
-        });
+});
