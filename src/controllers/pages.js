acb010cd43c67245ff7e1d3bca8ab007b562c5b5
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-04-29 17:24:56 +0900

    fix style

--- src/controllers/pages.js
+++ src/controllers/pages.js
@@ -2,12 +2,17 @@ var mongoose = require('mongoose');
 var pages = require('../models/pages');
 module.exports.controller = function(app) {
 
-    app.get('/', function(req, res) {
+    app.get('/', function(req,res) {
+        res.redirect('/top');
+    })
+
+    app.get('/top', function(req, res) {
         pages.load(function(err, pages) {
             if (err) console.log(err);
             res.render('index', {
                 pages: pages
             });
         });
-    })
+    });
+
 }
