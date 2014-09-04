268ade91d7de05d88b8e008bc95ef4718238ef0d
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-09-03 23:38:34 -0700

    move validity check

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -5,22 +5,45 @@ exports.username = function (req, res) {
 };
 
 exports.usernameAvailability = function (req, res) {
-    // TODO check validity
-    UserRepo.checkUsernameAvailability(req.body.username).then(
-        function(availability) {
-            res.send(availability);
+    var username = req.body.username;
+
+    if (!isValidUsername(username)) {
+        res.send(400, "Not a valid username :(");
+    }
+
+    UserRepo.checkUsernameAvailability(username).then(
+        function(isAvailable) {
+            res.send(isAvailable);
         },
         function(err) {console.log(err);}
     );
 };
 
 exports.setUsername = function (req, res) {
-    //TODO: check validity
-    //TODO: check availability
-    UserRepo.setUsername(req.body.username, req.user._id).then(
-        function(username) {
-            res.send(username);
+    var username = req.body.username;
+
+    if (!isValidUsername(username)) {
+        res.send(400, "Not a valid username :(");
+    }
+
+    UserRepo.checkUsernameAvailability(username).then(
+        function(isAvailable) {
+            if (isAvailable) {
+                UserRepo.setUsername(username, req.user._id).then(
+                    function(username) {
+                        res.send(username);
+                    },
+                    function(err) {console.log(err)}
+                );
+            } else {
+                res.send(400, "Username Not available :(");
+            }
         },
-        function(err) {console.log(err)}
+        function(err) {console.log(err);}
     );
+}
+
+isValidUsername = function(username) {
+    var validName = username.match(/^[a-z0-9_-]{3,15}$/);
+    return !!validName;
 }
