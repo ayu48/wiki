bb03c9ad31a2fdec735591bfba54537b8c0a730a
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-05 19:29:39 +0100

    Add travis-ci settings

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var jshint = require('gulp-jshint');
 gulp.task('lint', function() {
   return gulp.src(['./src/**/*.js', '*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('default', ['lint']);
