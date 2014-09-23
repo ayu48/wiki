c21d00bf7220cf8265068de1684d2a14cd0ec434
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-09-23 07:51:13 -0700

    add missing semicolon

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,7 +13,7 @@ exports.login = function(req, res) {
 exports.logout = function(req, res) {
     req.logout();
     res.redirect('/');
-}
+};
 
 exports.googleAuth = passport.authenticate('google');
 
